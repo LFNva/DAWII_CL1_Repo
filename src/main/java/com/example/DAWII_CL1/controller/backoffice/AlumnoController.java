9b286a430d101c45625dd7caacedb43766afc6e5
LFNva <luisfernandonovoa02@gmail.com>
2023-09-29 15:39:29 -0500

    Alumno Controller commit

--- src/main/java/com/example/DAWII_CL1/controller/backoffice/AlumnoController.java
+++ src/main/java/com/example/DAWII_CL1/controller/backoffice/AlumnoController.java
@@ -1,4 +1,46 @@
 package com.example.DAWII_CL1.controller.backoffice;
 
+import com.example.DAWII_CL1.model.Alumno;
+import com.example.DAWII_CL1.model.Especialidad;
+import com.example.DAWII_CL1.request.AlumnoRequest;
+import com.example.DAWII_CL1.response.ResultadoResponse;
+import com.example.DAWII_CL1.service.AlumnoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/alumno")
 public class AlumnoController {
+
+    @Autowired
+    private AlumnoService alumnoService;
+
+    @GetMapping("/listarAlumnos")
+    @ResponseBody
+    public List<Alumno> listarAlumnos(){
+        return alumnoService.listarAlumnos();
+    }
+
+    @PostMapping("/guardarAlumno")
+    @ResponseBody
+    public ResultadoResponse guardarAlumno(@RequestBody AlumnoRequest alumnoRequest){
+        return alumnoService.guardarAlumno(alumnoRequest);
+    }
+
+    @DeleteMapping("/eliminarAlumno")
+    @ResponseBody
+    public ResultadoResponse eliminarAlumno(@RequestBody AlumnoRequest alumnoRequest){
+        String mensaje = "Alumno Eliminado Correctamente";
+        Boolean respuesta = true;
+        try{
+            alumnoService.eliminarAlumno(alumnoRequest.getIdalumno());
+        }catch(Exception e){
+            mensaje = "Alumno no Eliminado";
+            respuesta = false;
+        }
+        return ResultadoResponse.builder().mensaje(mensaje).respuesta(respuesta).build();
+    }
 }
---
+++ src/main/java/com/example/DAWII_CL1/request/AlumnoRequest.java
@@ -0,0 +1,12 @@
+package com.example.DAWII_CL1.request;
+
+import lombok.Data;
+
+@Data
+public class AlumnoRequest {
+    private Integer idalumno;
+    private String apealumno;
+    private String nomalumno;
+    private String proce;
+    private Integer idesp;
+}
